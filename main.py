9ebadc6e4ad243f67ef4f268b694c273654f767e
User <sashapitr@me.com>
2019-09-30 01:25:36 +0300

    addded 2 headere files

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import glob
 import os
-import GPSC
 
-from PIL import ExifTags
 from PIL import Image
-from PIL.ExifTags import TAGS
-from PIL.ExifTags import GPSTAGS
 
+import coordinates_convector
+import redact_functions
 
 # path = str(input())
 # source_folder = str(input())
@@ -30,78 +28,26 @@ else:
 
 os.chdir(source_folder)
 
-
-# TODO: make function to get files' exif info
-def get_exif(required_file):
-    image = Image.open(required_file)
-    image.verify()
-    return image.getexif()
-
-
-def get_geo_info(required_exif):
-    if not required_exif:
-        return None
-
-    geo_info = {}
-    for (idx, tag) in TAGS.items():
-        if tag == 'GPSInfo':
-            if idx not in required_exif:
-                return None
-            for (key, val) in GPSTAGS.items():
-                if key in required_exif[idx]:
-                    geo_info[val] = required_exif[idx][key]
-
-    return geo_info
-
-
-def get_rotation(orientation):
-    if orientation == 1:
-        pass
-    if orientation == 2:
-        return -90
-    if orientation == 3:
-        return -90
-    if orientation == 4:
-        return -90
-    if orientation == 5:
-        return -90
-    if orientation == 6:
-        return -90
-    if orientation == 7:
-        return -90
-    if orientation == 8:
-        return -90
-    return 0
-
-
 for file in glob.glob('*.JPG'):
     with Image.open(file) as original_image:
-        exif = get_exif(file)
-        geotags = get_geo_info(exif)
-
+        exif = redact_functions.get_exif(file)
+        geotags = redact_functions.get_geo_info(exif)
+        required_rotation = redact_functions.get_orientation(file)
+        # print(required_rotation)
         # TODO: add recording to file and check rotating function
         if geotags:
-            coordinates = GPSC.get_coordinates(geotags)
-            print(coordinates)
+            coordinates = coordinates_convector.get_coordinates(geotags)
+            redact_functions.rotating_the_image(source_folder, file, destination_folder)
+            # print(coordinates)
 
-
-
-        if 'GPSInfo' in metadata_of_original_image:
-            # TODO: WRITE COORDINATES HERE IN GEOJSON FILE
-            my_file = open(destination_folder + name_of_the_geojson_file, "w+")
-            if 'Latitude:' in metadata_of_original_image:
-                my_file.write("Latitude:" + metadata_of_original_image['GPSInfo'])
-            elif 'Longitude:' in metadata_of_original_image:
-                my_file.write("Longitude:" + metadata_of_original_image['GPSInfo'])
-
-            changing_orientation_index = 'Orientation'
-            if changing_orientation_index in metadata_of_original_image:
-                orientation_index = metadata_of_original_image['Orientation']
-                rotation_angle = get_rotation(orientation_index)
-                rotated_image = original_image.rotate(rotation_angle)
-                resized_image = rotated_image.resize((200, 200))
-                resized_image.save(destination_folder + file)
-            else:
-                pass
+            # changing_orientation_index = 'Orientation'
+            # if changing_orientation_index in metadata_of_original_image:
+            #     orientation_index = metadata_of_original_image['Orientation']
+            #     rotation_angle = get_rotation(orientation_index)
+            #     rotated_image = original_image.rotate(rotation_angle)
+            #     resized_image = rotated_image.resize((200, 200))
+            #     resized_image.save(destination_folder + file)
+            # else:
+            #     pass
         else:
             pass
---
+++ redact_functions.py
@@ -0,0 +1,61 @@
+from PIL import ExifTags
+from PIL import Image
+from PIL.ExifTags import GPSTAGS
+from PIL.ExifTags import TAGS
+
+
+def get_exif(required_file):
+    image = Image.open(required_file)
+    image.verify()
+    return image.getexif()
+
+
+def get_geo_info(required_exif):
+    if not required_exif:
+        return None
+
+    geo_info = {}
+    for (idx, tag) in TAGS.items():
+        if tag == 'GPSInfo':
+            if idx not in required_exif:
+                return None
+            for (key, val) in GPSTAGS.items():
+                if key in required_exif[idx]:
+                    geo_info[val] = required_exif[idx][key]
+
+    return geo_info
+
+
+def get_orientation(required_file):
+    with Image.open(required_file) as required_image:
+        metadata_of_original_image = {
+            ExifTags.TAGS[k]: v
+            for k, v in required_image.getexif().items()
+            if k in ExifTags.TAGS
+        }
+        if 'Orientation' in metadata_of_original_image:
+            orientation_index = metadata_of_original_image['Orientation']
+            return orientation_index
+        elif 'Orientation' not in metadata_of_original_image:
+            return None
+
+
+def get_rotate(orientation):
+    if orientation == 1:
+        return 0
+    elif orientation == 3:
+        return -180
+    elif orientation == 6:
+        return -270
+    elif orientation == 8:
+        return -90
+    return 0
+
+
+def rotating_the_image(image_path, required_file, saving_path):
+    image_obj = Image.open(image_path + required_file)
+    orientation = get_orientation(required_file)
+    degrees = get_rotate(orientation)
+    rotated_image = image_obj.rotate(degrees)
+    rotated_image.save(saving_path)
+    return 0
