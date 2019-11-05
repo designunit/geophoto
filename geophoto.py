c21b21bba7bed3d6c378587e47d51b3002f6c182
Alex Petrosyan <sashapitr@me.com>
2019-11-05 19:07:46 +0300

    __main__

--- geophoto.py
+++ geophoto.py
@@ -9,38 +9,39 @@ import imglib
 import geojson
 
 
-source_folder = sys.argv[1]
-destination_folder = sys.argv[2]
-full_name_of_the_geojson_file = os.path.join(destination_folder, 'dataset.geojson')
-url_base = sys.argv[3]
-size = (int(sys.argv[4]), int(sys.argv[4]))
+if __name__ == '__main__':
+    source_folder = sys.argv[1]
+    destination_folder = sys.argv[2]
+    full_name_of_the_geojson_file = os.path.join(destination_folder, 'dataset.geojson')
+    url_base = sys.argv[3]
+    size = (int(sys.argv[4]), int(sys.argv[4]))
 
-if not os.path.exists(destination_folder):
-    os.mkdir(destination_folder)
+    if not os.path.exists(destination_folder):
+        os.mkdir(destination_folder)
 
-images = []
-counter = 0
+    images = []
+    counter = 0
 
-for file in glob.glob(os.path.join(source_folder, '*.JPG')):
-    file_with_normpath = os.path.normpath(file)
-    image = Image.open(file)
-    image.load()
-    exif = imglib.get_exif(image)
-    geotags = imglib.get_geo_info(exif)
-    if geotags:
-        coordinates = coordlib.get_coordinates(geotags)
-        path = os.path.join(destination_folder, os.path.basename(file))
-        imglib.saving_modified_img(image, path, size)
+    for file in glob.glob(os.path.join(source_folder, '*.JPG')):
+        file_with_normpath = os.path.normpath(file)
+        image = Image.open(file)
+        image.load()
+        exif = imglib.get_exif(image)
+        geotags = imglib.get_geo_info(exif)
+        if geotags:
+            coordinates = coordlib.get_coordinates(geotags)
+            path = os.path.join(destination_folder, os.path.basename(file))
+            imglib.process_image(image, path, size)
 
-        images.append({
-            'id': counter,
-            'url': url_base + file,
-            'coordinates': coordinates,
-            'value': 1
-        })
-        counter += 1
-    else:
-        pass
+            images.append({
+                'id': counter,
+                'url': url_base + file,
+                'coordinates': coordinates,
+                'value': 1
+            })
+            counter += 1
+        else:
+            pass
 
-features = geojson.create_geojson(images)
-geojson.save_json(full_name_of_the_geojson_file, features)
+    features = geojson.create_geojson(images)
+    geojson.save_json(full_name_of_the_geojson_file, features)
