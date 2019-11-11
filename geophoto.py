6880d9662f948a075e011146ebaa9ce6a4af4c91
Alex Petrosyan <sashapitr20@gmail.com>
2019-11-11 20:10:18 +0300

    normal line

--- geophoto.py
+++ geophoto.py
@@ -10,28 +10,26 @@ import geojson
 
 
 def main():
-    source_folder = sys.argv[1]
-    source_folder_expanded = os.path.expanduser(source_folder)
-    destination_folder = sys.argv[2]
-    destination_folder_expanded = os.path.expanduser(destination_folder)
-    full_name_of_the_geojson_file = os.path.join(destination_folder_expanded, 'dataset.geojson')
+    source_folder = os.path.expanduser(sys.argv[1])
+    destination_folder = os.path.expanduser(sys.argv[2])
+    full_name_of_the_geojson_file = os.path.join(destination_folder, 'dataset.geojson')
     url_base = sys.argv[3]
     size = (int(sys.argv[4]), int(sys.argv[4]))
 
-    if not os.path.exists(destination_folder_expanded):
-        os.mkdir(destination_folder_expanded)
+    if not os.path.exists(destination_folder):
+        os.mkdir(destination_folder)
 
     images = []
     counter = 0
 
-    for file in glob.glob(os.path.join(source_folder_expanded, '*.JPG')):
+    for file in glob.glob(os.path.join(source_folder, '*.JPG')):
         image = Image.open(file)
         image.load()
         exif = imglib.get_exif(image)
         geotags = imglib.get_geo_info(exif)
         if geotags:
             coordinates = coordlib.get_coordinates(geotags)
-            path = os.path.join(destination_folder_expanded, os.path.basename(file))
+            path = os.path.join(destination_folder, os.path.basename(file))
             imglib.process_image(image, path, size)
 
             images.append({
